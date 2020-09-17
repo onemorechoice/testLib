0f12396dab034f72cc5c7de6fb6fc1257b9d308d
richar_ye <496008162@qq.com>
2020-09-17 20:29:24 +0800

    添加测试库

--- app/src/main/java/com/yjf/testlib/MainActivity.kt
+++ app/src/main/java/com/yjf/testlib/MainActivity.kt
@@ -2,10 +2,12 @@ package com.yjf.testlib
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.yjf.library.testLog
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        testLog("测试测试")
     }
 }
